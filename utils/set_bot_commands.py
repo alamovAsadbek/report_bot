b3a411b8508e73a4bedee59a524e02af2cdb2481
alamovAsadbek <alamovasad@gmail.com>
2025-04-29 14:11:34 +0500

    change

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,5 +7,7 @@ async def set_default_commands(bot: Bot):
             types.BotCommand(command="start", description="Start to use bot 🚀️️️️️️"),
             types.BotCommand(command="help", description="Find all features 🤖"),
             types.BotCommand(command="feedback", description="Send feedback to admin 📝"),
+            types.BotCommand(command="users", description="For admins"),
+            types.BotCommand(command="send", description="For admins"),
         ]
     )
